a8d03c07c0bb9c811b4b3c8eca78d58e2147e353
Zilan1 <79017444+Zilan1@users.noreply.github.com>
2022-03-26 15:32:40 +0300

    sum

---
+++ proje52.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+  int main() {
+   
+   // 3 basamakli sayilarin toplamin bulan program
+   
+    //int birler,onlar,yuzler,toplam,sayi;
+   
+    //printf("Sayiyi Giriniz: ");
+    //scanf("%d",&sayi);
+   
+    //birler=sayi%10;
+    //yuzler=sayi/100;
+    //onlar=(sayi/10)%10;
+   
+    //toplam=birler+onlar+yuzler;
+   
+    //printf("Toplam: %d",toplam);
+   
+   
+   
+   
+   
+   
+   // 1 yil icinde otelde kalan kisi sayisi
+   
+    //int ilkbahar,yaz,sonbahar,kis,toplam;
+    //kis=320; 
+    //ilkbahar=kis/4;
+    //yaz=ilkbahar*8;
+    //sonbahar=yaz/10;
+  
+    //toplam=ilkbahar+yaz+sonbahar+kis;
+    
+    //printf("Toplam: %d",toplam);
+
+
+
+
+    // Klavyeden girilen iki farkli sayiyi toplama
+
+	//int s1,s2,toplam=0,buyuk,kucuk,i;
+    
+	//yeniden: 
+	 	
+	//printf("Birinci Sayiyi Giriniz: ");
+	//scanf("%d",&s1);
+	
+	//printf("Ikinci Sayiyi Giriniz: ");
+	//scanf("%d",&s2);
+
+    //if(s1==s2)
+	//{
+	   //goto yeniden;
+	//}	
+	//else
+	//{
+		//if(s1>s2)
+		//{
+			//buyuk=s1;
+			//kucuk=s2;
+		//}
+		//else
+		//{
+			//kucuk=s1;
+			//buyuk=s2;
+		//}
+	//}
+	//for(i=kucuk;i<=buyuk;i++)
+	//{
+		//toplam=toplam+i;
+	//}
+	
+	//printf("Toplam: %d",toplam);
+	
+	
+	
+	
+	
+	// El ve Bagaj ek tutar hesaplama
+	
+	//int normal,el,normaltutar,eltutar,toplam;
+	
+	//printf("El Bagaj Kg: ");
+	//scanf("%d",&el);
+	
+	//printf("Normal Bagaj Kg: ");
+	//scanf("%d",&normal);
+	
+	//if(normal<15)
+	//{
+		//normaltutar=0;
+	//}
+	//else
+	//{
+		//normaltutar=(normal-15)*5;
+	//}	
+	//if(el<8)
+	//{
+		//eltutar=0;
+	//}
+	//else
+	//{
+		//eltutar=(el-8)*4;
+	//}
+	
+	//toplam=normaltutar+eltutar;
+	
+	//printf("Toplam Ek Tutariniz: %d TL ",toplam);
+	
+	return 0;
+}
